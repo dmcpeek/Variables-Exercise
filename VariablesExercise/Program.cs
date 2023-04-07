14d46ed3ce57b4755746be1a5b40f2afb56ca805
Doug McPeek <dmcpeek@outlook.com>
2023-04-07 12:19:36 -0600

    initial checkin

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Lissa";
+            int ageMonths = 9;
+            char letter = 'A';
+            bool mouthy = true;
+            double cupsOfFood = 2.5;
+            decimal joyFactor = 2358m;
+            Console.WriteLine($"My dog's name is {dogName} and she is {ageMonths} months old. {letter}nd, it is {mouthy} that she is very mouthy. She eats {cupsOfFood} cups of food per day. She being us a joyfactor of {joyFactor} out of 10!");
         }
     }
 }
